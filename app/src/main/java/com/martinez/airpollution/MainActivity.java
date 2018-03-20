c758a9b1eaeaa3382578f46119dd85f9d320b0ae
martinez1910 <martinez1910@users.noreply.github.com>
2018-03-20 16:44:20 +0100

    Simplify AirStations design
    - Use wrapper classes
    - Remove old useless code
    - Remove old useless elements from MainActivity's GUI

--- app/src/main/java/com/martinez/airpollution/MainActivity.java
+++ app/src/main/java/com/martinez/airpollution/MainActivity.java
@@ -1,23 +1,15 @@
 package com.martinez.airpollution;
 
-import android.content.Context;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Html;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.Toast;
 
 import com.martinez.airpollution.airsations.AirStation;
-import com.martinez.airpollution.airsations.AirStationType1;
-import com.martinez.airpollution.airsations.AirStationType2;
-import com.martinez.airpollution.airsations.AirStationType2Doubles;
-import com.martinez.airpollution.airsations.AirStationType3;
-import com.martinez.airpollution.airsations.AirStationType4;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -26,7 +18,6 @@ import org.json.JSONObject;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.Serializable;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.ArrayList;
@@ -40,34 +31,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //addListeners();REMOVE
-
         getAirStationsData();
     }
 
-    /*REMOVE
-    private void addListeners(){
-        ((ListView) findViewById(R.id.listView)).setOnItemClickListener(
-                new AdapterView.OnItemClickListener(){
-                    @Override
-                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-
-                    }
-                });
-    }*/
-
-    /*REMOVE
-    public void onButtonClick(View w){
-        Toast.makeText(this.getApplicationContext(), getString(R.string.toast_updating), Toast.LENGTH_LONG).show();
-        new Communicator().execute("http://opendata.gijon.es/descargar.php?id=1&tipo=JSON");
-    }*/
-
-    //Remove onButtonClick() when this is implemented
     public void onImageClick(View w){
-        //if info already parsed show data, wait (and show loading gif) otherwise.
-//        while(airStations.size() == 0 || airStations.get(airStations.size()-1) == null) //avoids race condition but may loop forever(?)[Check if that's right]
-//            Toast.makeText(this, R.string.toast_loading, Toast.LENGTH_LONG).show(); //Too much toast in my jam
-
         switch (w.getId()){
             case R.id.imageViewCircleAvdaConstitucion:
                 startActivity(new Intent(this, StationActivity.class).putExtra("airStation", airStations.get(0)));
@@ -124,48 +91,13 @@ public class MainActivity extends AppCompatActivity {
                 for(int i=0; i<jsonArray.length(); i++){
                     if(counter != 0 && airStations.get(counter-1).getEstacion() == jsonArray.getJSONObject(i).getInt("estacion"))//Just add the latest record of each station.
                         continue;
-                    AirStation airStation = createAirStation(jsonArray.getJSONObject(i));
-                    airStations.add(airStation);
+                    //AirStation airStation = createAirStation(jsonArray.getJSONObject(i));//REMOVE
+                    airStations.add(new AirStation(jsonArray.getJSONObject(i)));
                     counter++;
                 }
-                loadData(); //REMOVE
             } catch (JSONException e) {
                 e.printStackTrace();
             }
-        }
-
-        private AirStation createAirStation(JSONObject j){
-            try {
-                Context context = getApplicationContext();
-                int estacion = j.getInt("estacion");
-                String titulo = j.getString("titulo"), fechasolar_utc_ = j.getString("fechasolar_utc_"), pm10 = j.getString("pm10"), pm25 = j.getString("pm25");
-                double latitud = j.getDouble("latitud"), longitud = j.getDouble("longitud");
-                switch(estacion){
-                    case 1:
-                        return new AirStationType1(context, estacion, titulo, latitud, longitud, fechasolar_utc_, pm10, pm25,  j.getInt("so2"), j.getInt("no"), j.getInt("no2"), j.getDouble("co"), j.getInt("o3"), j.getInt("dd"), j.getDouble("vv"), j.getDouble("tmp"), j.getInt("hr"), j.getInt("prb"), j.getInt("rs"), j.getInt("ll"), j.getDouble("ben"), j.getDouble("tol"), j.getDouble("mxil"));
-                    case 2:
-                    case 3:
-                        return new AirStationType2(context, estacion, titulo, latitud, longitud, fechasolar_utc_, pm10, pm25, j.getInt("so2"), j.getInt("no"), j.getInt("no2"), j.getDouble("co"), j.getInt("o3"));
-                    case 10:
-                        return new AirStationType3(context, estacion, titulo, latitud, longitud, fechasolar_utc_, pm10, pm25, j.getInt("so2"), j.getInt("no"), j.getInt("no2"), j.getInt("o3"), j.getInt("dd"), j.getDouble("vv"), j.getDouble("tmp"), j.getInt("hr"), j.getInt("prb"), j.getInt("rs"), j.getInt("ll"));
-                    case 4:
-                        return new AirStationType2Doubles(context, estacion, titulo, latitud, longitud, fechasolar_utc_, pm10, pm25, j.getDouble("so2"), j.getDouble("no"), j.getDouble("no2"), j.getDouble("co"), j.getDouble("o3"));
-                    case 11:
-                        return new AirStationType4(context, estacion, titulo, latitud, longitud, fechasolar_utc_, pm10, pm25, j.getInt("no"), j.getInt("no2"), j.getInt("co"));
-                }
-            }catch(JSONException e){
-                e.printStackTrace();
-            }
-            return null;
-        }
-
-        //REMOVE
-        private void loadData(){
-            AirStation[] data = new AirStation[airStations.size()];
-            for (int i = 0; i < data.length; i++)
-                data[i] = airStations.get(i);
-
-            ((ListView) findViewById(R.id.listView)).setAdapter(new ArrayAdapter<AirStation>(MainActivity.this, android.R.layout.simple_list_item_1, data));
         }
     }
 }
--- app/src/main/java/com/martinez/airpollution/airsations/AirStation.java
+++ app/src/main/java/com/martinez/airpollution/airsations/AirStation.java
@@ -12,37 +12,58 @@ import org.json.JSONObject;
 import java.io.Serializable;
 import java.io.UnsupportedEncodingException;
 
-public abstract class AirStation implements Serializable{
-    protected Context context;
-    private int estacion;
+public class AirStation implements Serializable{
+    private Integer estacion;
     private String titulo, fechasolar_utc_, pm10, pm25;
-    private double latitud, longitud;
+    private Double latitud, longitud, so2, no, no2, co, o3, dd, vv, tmp, hr, prb, rs, ll, ben, tol, mxil;
 
-    public AirStation(Context context, int estacion, String titulo,  double latitud, double longitud, String fechasolar_utc_, String pm10, String pm25) {
-        this.context = context;
-        this.estacion = estacion;
-        this.titulo = titulo;
-        this.latitud = latitud;
-        this.longitud = longitud;
-        this.fechasolar_utc_ = fechasolar_utc_;
-        this.pm10 = pm10;
-        this.pm25 = pm25;
+    public AirStation(JSONObject jsonObject){
+        try{
+            this.estacion = jsonObject.getInt("estacion");
+            this.titulo = jsonObject.getString("titulo");
+            this.latitud = jsonObject.getDouble("latitud");
+            this.longitud = jsonObject.getDouble("longitud");
+            this.fechasolar_utc_ = jsonObject.getString("fechasolar_utc_");
+            this.longitud = jsonObject.getDouble("longitud");
+            this.so2 = jsonObject.getDouble("so2");
+            this.no = jsonObject.getDouble("no");
+            this.no2 = jsonObject.getDouble("no2");
+            this.co = jsonObject.getDouble("co");
+            this.pm10 = jsonObject.getString("pm10");
+            this.o3 = jsonObject.getDouble("o3");
+            this.dd = jsonObject.getDouble("dd");
+            this.vv = jsonObject.getDouble("vv");
+            this.tmp = jsonObject.getDouble("tmp");
+            this.hr = jsonObject.getDouble("hr");
+            this.prb = jsonObject.getDouble("prb");
+            this.rs = jsonObject.getDouble("rs");
+            this.ll = jsonObject.getDouble("ll");
+            this.ben = jsonObject.getDouble("ben");
+            this.tol = jsonObject.getDouble("tol");
+            this.mxil = jsonObject.getDouble("mxil");
+            this.pm25 = jsonObject.getString("pm25");
+        }catch(JSONException e){
+            e.printStackTrace();
+        }
     }
 
+    /*Move this method to StationActivity
+    private String formatDate(String date){
+        try{
+            String year = date.substring(0, 4);
+            String month = date.substring(5, 7);
+            String day = date.substring(8, 10);
+            String hour = date.substring(11, 13);
+            String minute = date.substring(14, 16);
+            String second = date.substring(17, 19);
 
-    protected String formatDate(String date){
-        String year = date.substring(0, 4);
-        String month = date.substring(5, 7);
-        String day = date.substring(8, 10);
-        String hour = date.substring(11, 13);
-        String minute = date.substring(14, 16);
-        String second = date.substring(17, 19);
+            return day +"/" +month +"/" +year + " " +context.getString(R.string.text_before_time) +" " +hour +":" +minute +":" +second;
+        }catch(IndexOutOfBoundsException e){
+            return date;
+        }
+    }*/
 
-        return day +"/" +month +"/" +year + " " +context.getString(R.string.text_before_time) +" " +hour +":" +minute +":" +second;
-    }
-
-    public int getEstacion() {
-        return estacion;
+    public Integer getEstacion() {        return estacion;
     }
 
     public String getTitulo() {
@@ -61,11 +82,71 @@ public abstract class AirStation implements Serializable{
         return pm25;
     }
 
-    public double getLatitud() {
+    public Double getLatitud() {
         return latitud;
     }
 
-    public double getLongitud() {
+    public Double getLongitud() {
         return longitud;
+    }
+
+    public Double getSo2() {
+        return so2;
+    }
+
+    public Double getNo() {
+        return no;
+    }
+
+    public Double getNo2() {
+        return no2;
+    }
+
+    public Double getCo() {
+        return co;
+    }
+
+    public Double getO3() {
+        return o3;
+    }
+
+    public Double getDd() {
+        return dd;
+    }
+
+    public Double getVv() {
+        return vv;
+    }
+
+    public Double getTmp() {
+        return tmp;
+    }
+
+    public Double getHr() {
+        return hr;
+    }
+
+    public Double getPrb() {
+        return prb;
+    }
+
+    public Double getRs() {
+        return rs;
+    }
+
+    public Double getLl() {
+        return ll;
+    }
+
+    public Double getBen() {
+        return ben;
+    }
+
+    public Double getTol() {
+        return tol;
+    }
+
+    public Double getMxil() {
+        return mxil;
     }
 }
